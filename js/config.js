f7d950c6b17dc1aba40989abdc810c41c2667f80
Sipho Hlebeya <siphohlebeya7@gmail.com>
2026-09-24 17:45:50 +0200

    removed config.js from gitignore

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// ==========================================
+// 1. BACKEND CONNECTION (SUPABASE)
+// ==========================================
+// Initialize the connection to our Supabase database using project credentials.
+export const  ENV = {
+    SUPABASE_URL : "https://cmkfgrtfynvtwwmliycr.supabase.co",
+    SUPABASE_ANON : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNta2ZncnRmeW52dHd3bWxpeWNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3NDM5NjAsImV4cCI6MjEwMjMxOTk2MH0.MYl5tArwgb6Sv-oQkXBRg79ay46Cnw9nit_PbwhBHeo"
+};
